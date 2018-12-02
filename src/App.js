81349daf8a6b0efcdc47233eacb67fc255476889
Bo Pace <bpace@instructure.com>
2018-12-02 13:38:10 -0700

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import axios from 'axios'
 import FlowerShopScreenWrapper from './screens/FlowerShopScreenWrapper'
 
-class App extends Component {
+// screens
+const flowerShopScreen = () => (
+  <FlowerShopScreenWrapper />
+)
+
+const placeOrderScreen = () => (
+  <div>Coming soon!</div>
+)
+
+const myOrdersScreen = () => (
+  <div>Coming soon!</div>
+)
+
+export default class App extends Component {
   state = {
     data: [],
     id: 0,
@@ -28,59 +42,23 @@ class App extends Component {
     }
   }
 
-  getDataFromDb = () => {
-    fetch('/api/getData')
-      .then(data => data.json())
-      .then(res => this.setState({ data: res.data }))
-  }
-
-  putDataToDB = message => {
-    let currentIds = this.state.data.map(data => data.id)
-    let idToBeAdded = 0
-    while (currentIds.includes(idToBeAdded)) {
-      idToBeAdded++
-    }
-
-    axios.post('/api/putData', {
-      id: idToBeAdded,
-      message: message
-    })
-  }
-
-  deleteFromDB = idToDelete => {
-    let objIdToDelete = null
-    this.state.data.forEach(dat => {
-      if (dat.id === idToDelete) {
-        objIdToDelete = dat._id
-      }
-    })
-
-    axios.delete('/api/deleteData', {
-      data: {
-        id: objIdToDelete
-      }
-    })
-  }
-
-  updateDB = (idToUpdate, updateToApply) => {
-    let objIdToUpdate = null
-    this.state.data.forEach(dat => {
-      if (dat.id === idToUpdate) {
-        objIdToUpdate = dat._id
-      }
-    })
-
-    axios.post('/api/updateData', {
-      id: objIdToUpdate,
-      update: { message: updateToApply }
-    })
-  }
-
   render() {
     return (
-      <div>
-        <FlowerShopScreenWrapper />
-      </div>
+      <Router>
+        <div>
+          <FlowerShopScreenWrapper />
+        </div>
+
+        <Route path='/shops' exact
+          component={flowerShopScreen}
+        />
+        <Route path='/place-order' exact
+          component={placeOrderScreen}
+        />
+        <Route path='/my-orders' exact
+          component={myOrdersScreen}
+        />
+      </Router>
     )
   }
 
@@ -144,6 +122,54 @@ class App extends Component {
         </div>
       </div>
     )
+  }
+
+  getDataFromDb = () => {
+    fetch('/api/getData')
+      .then(data => data.json())
+      .then(res => this.setState({ data: res.data }))
+  }
+
+  putDataToDB = message => {
+    let currentIds = this.state.data.map(data => data.id)
+    let idToBeAdded = 0
+    while (currentIds.includes(idToBeAdded)) {
+      idToBeAdded++
+    }
+
+    axios.post('/api/putData', {
+      id: idToBeAdded,
+      message: message
+    })
+  }
+
+  deleteFromDB = idToDelete => {
+    let objIdToDelete = null
+    this.state.data.forEach(dat => {
+      if (dat.id === idToDelete) {
+        objIdToDelete = dat._id
+      }
+    })
+
+    axios.delete('/api/deleteData', {
+      data: {
+        id: objIdToDelete
+      }
+    })
+  }
+
+  updateDB = (idToUpdate, updateToApply) => {
+    let objIdToUpdate = null
+    this.state.data.forEach(dat => {
+      if (dat.id === idToUpdate) {
+        objIdToUpdate = dat._id
+      }
+    })
+
+    axios.post('/api/updateData', {
+      id: objIdToUpdate,
+      update: { message: updateToApply }
+    })
   }
 }
 
